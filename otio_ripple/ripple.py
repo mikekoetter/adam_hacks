360acf264100784f4d38f26b5397f4ff73a11804
Michael Koetter <michael.koetter@disneyanimation.com>
2020-11-05 21:04:06 -0800

    feat(nucoda_stack): Ripple and add Nucoda LUT

--- otio_ripple/ripple.py
+++ otio_ripple/ripple.py
@@ -2,7 +2,7 @@ import opentimelineio as otio
 
 EDIT_RATE = 24
 HOUR = 3600 * EDIT_RATE
-timeline = otio.adapters.read_from_file("sample_data/sample.edl")
+timeline = otio.adapters.read_from_file("sample_data/sample2.edl")
 for clip in timeline.each_clip():
     ripple = -HOUR + 100
 
@@ -13,6 +13,26 @@ for clip in timeline.each_clip():
         ripple = 100
 
     end_frame = start_frame + clip.source_range.duration.value + ripple
+    edl_meta = clip.metadata.get('cmx_3600',{})
+    
+    nucoda_stack = [
+        'NUCODA_LAYER Log',
+        'NUCODA_LAYER Log2',
+        'NUCODA_LAYER Log3',
+        'NUCODA_LAYER Log4',
+        'NUCODA_LAYER Log5',
+        'NUCODA_LAYER Log6',
+    ]
+
+    comments = edl_meta.get('comments', [])
+
+    for comment in comments:
+        if 'REEL:' in comment:
+            _, reel_name = comment.split(": ")
+            lut_layer = 'NUCODA_LAYER GT_LUT -effect NucodaCMSPath -lut T:\\luts\\GoodTrouble\\{}.cube'.format(reel_name)
+            nucoda_stack.append(lut_layer)
+
+    comments.extend(nucoda_stack)
 
     clip.source_range = otio.opentime.range_from_start_end_time(
         otio.opentime.from_frames(start_frame + ripple, EDIT_RATE),
